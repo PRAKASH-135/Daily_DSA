8ba3e367ba5c1477696c993953855a0d6837cd35
Prakash Tengunti <prakashs.23.becs@acharya.ac.in>
2026-08-29 20:08:44 +0530

    Time: 1 ms (92.94%), Space: 47.2 MB (53.61%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,19 +1,23 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int n = nums.length;
-        int result = Integer.MIN_VALUE;
-        
-        for(int i=0 ; i<n ; i++)
+        int prefix = 1; 
+        int suffix = 1; 
+        int max = Integer.MIN_VALUE;
+        for(int i=0 ; i<nums.length ; i++)
         {
-                int prod = 1;
+           if(prefix == 0 )
+           {
+             prefix = 1;
+           }
+           if(suffix == 0)
+           {
+            suffix = 1;
+           }
 
-            for(int j=i ; j<n ; j++)
-            {
-                    prod = nums[j]*prod;
-
-             result = Math.max(result , prod);
-            }
+           prefix = prefix * nums[i];
+           suffix = suffix * nums[nums.length - 1 -i];
+            max =  Math.max(max , Math.max(prefix , suffix));
         }
-        return result;
+       return  max; 
     }
 }
